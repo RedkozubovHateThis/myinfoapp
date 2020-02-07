08f0bf0295253c65fd84992d3e2f6b5d40696092
Антон Редкозубов <uacreez@gmail.com>
2020-02-07 21:08:03 +0400

    Initial Commit

---
+++ myinfoapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  myinfoapp
+//
+//  Created by Anton Redkozubov on 07.02.2020.
+//  Copyright © 2020 Anton Redkozubov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
